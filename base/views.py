0b40d2abbc1263611547c323140ab6f8146f8b35
Biswas-Souvik <biswassouvik284@gmail.com>
2024-09-11 14:40:13 +0530

    Dynamic Templates: passing values to templates

--- base/views.py
+++ base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': "let's learn Python"},
+    {'id': 2, 'name': "How I learn Python"},
+    {'id': 3, 'name': "Python 201"},
+]
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'room.html', {'topics': rooms})
