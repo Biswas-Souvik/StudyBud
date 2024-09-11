f676a8acb7c6ebc5955065c0c3b73f89a2047085
Biswas-Souvik <biswassouvik284@gmail.com>
2024-09-11 14:55:11 +0530

    Move dynamic values to home page

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ rooms = [
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {'topics': rooms})
 
 def room(request):
-    return render(request, 'base/room.html', {'topics': rooms})
+    return render(request, 'base/room.html')
