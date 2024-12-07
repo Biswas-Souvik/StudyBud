7608d084cc80f8848fed0a43efd6827a01c249c8
Biswas-Souvik <biswassouvik284@gmail.com>
2024-12-07 10:55:52 +0530

    Added Custom User Registration

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,7 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField, EmailField
 from .models import Room
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class RoomForm(ModelForm):
@@ -14,3 +15,14 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    first_name = CharField(max_length=30, required=True)
+    last_name = CharField(max_length=30, required=True)
+    email = EmailField(max_length=254, required=False)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
--- base/views.py
+++ base/views.py
@@ -3,11 +3,10 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, CustomUserCreationForm
 
 
 # Create your views here.
@@ -45,10 +44,10 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.capitalize()
